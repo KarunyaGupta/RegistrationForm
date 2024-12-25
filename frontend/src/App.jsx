555bc2d623055cfce4317fa60c0017bc2f690194
KarunyaGupta <mails.karunyagupta@gmail.com>
2024-12-25 11:59:27 +0530

    update folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './pages/RegistrationForm'
 import DataTable from './pages/Datatable'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [btn, setBtn] = useState(false);
+  
+  
 
   return (
     <>
-    <RegistrationForm/>
-    <DataTable/>
+    
+    <button onClick={() => setBtn(!btn)}>
+        {
+          btn ? "Registration Page" : "Data Page"
+        }
+      </button>
+
+    {
+      btn ? <DataTable/> : <RegistrationForm/>
+    }
+
+    {/* Show data in a single page */}
+    {/* <RegistrationForm/>
+    <DataTable/> */}
     </>
   )
 }
